ddc7e1f35ca381e4b74dec88e4ff02dc346fe3ad
Jed Cua <jonedwardcua@gmail.com>
2016-09-20 14:12:15 +0800

    Solved Problem 22

--- Prob22.hs
+++ Prob22.hs
@@ -1,3 +1,5 @@
+module Prob22 where
+
 -- Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over 
 -- five-thousand first names, begin by sorting it into alphabetical order. Then working out the 
 -- alphabetical value for each name, multiply this value by its alphabetical position in the list 
@@ -8,17 +10,39 @@
 -- of 938 × 53 = 49714.
 
 -- What is the total of all the name scores in the file?
-import Data.List (elemIndex)
-import Control.Monad (mapM)
+import Data.List (elemIndex, sort)
+import Data.List.Split (splitOn)
 import Data.Maybe (fromJust)
+import Control.Monad.State
 
 type Letter = Char
 
+letters :: String
 letters = ['A'..'Z']
 
 value :: Letter -> Int
 value l = (+1) . fromJust $ l `elemIndex` letters
 
 nameScore :: Int -> [Letter] -> Int
-nameScore pos letters = pos * sum values where
-    values = map value letters
+nameScore pos ls = pos * sum values where
+    values = map value ls
+
+getScore :: String -> State Int Int
+getScore name = do
+        pos <- get
+        let score = nameScore pos name
+        modify (+1)
+        return score
+
+parseNames :: FilePath -> IO [String]
+parseNames file = do
+        names <- readFile file
+        let names' = splitOn "," names
+            names'' = map read names' :: [String]
+        return $ sort names''
+
+answer :: IO Int
+answer = do
+    names <- parseNames "res/p022_names.txt"
+    let score = evalState (mapM getScore names) 1
+    return $ sum score
